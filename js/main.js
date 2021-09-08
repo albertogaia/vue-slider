e56fc5960fb4a5176b0daf2d3588c67a6ca17670
albertogaia <alberto.gaia95@gmail.com>
2021-09-08 14:17:59 +0200

    Slider e frecce

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 Vue.config.devtools = true;
 
 const app = new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        counterPhoto: 0,
+        photos: [
+            'img/django.jpg',
+            'img/hateful-eight.jpg',
+            'img/Kill-Bill-Vol-2.jpg',
+            'img/resevoir-dogs.jpg',
+        ]
+    },
+    methods: {
+        prevPhoto(){
+            this.counterPhoto -= 1;
+            if(this.counterPhoto < 0)
+                this.counterPhoto = this.photos.length - 1
+            
+        },
+        nextPhoto(){
+            this.counterPhoto += 1;
+            if(this.counterPhoto == this.photos.length)
+                this.counterPhoto = 0;
+            
+        }
+    }
 })
